b41abb49154c98876dfd35fa4d0ac1d6c2fd2a68
Michael Billard <angel125@spellflight.com>
2022-06-15 18:12:31 -0700

    Initial Commit

---
+++ source/WildBlueCore/PartModules/Variants/ModuleResourceVariants.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Buffalo2.Wrappers;
+
+namespace WildBlueCore
+{
+    /// <summary>
+    /// A small helper class to update a part's resources when a part variant is applied.
+    /// </summary>
+    public class ModuleResourceVariants : BasePartModule
+    {
+        #region Constants
+        const string kResourceName = "resourceName";
+        const string kAmount = "amount";
+        const string kMaxAmount = "maxAmount";
+        const string kPackedVolumeLimit = "packedVolumeLimit";
+        const string kResourceNode = "RESOURCE";
+        const string kVariantNode = "VARIANT";
+        const string kName = "name";
+        const string kVolumeMultiplier = "volumeMultiplier";
+        #endregion
+
+        #region Fields
+        /// <summary>
+        /// Resource volume size, in liters, per unit of volume. When the extra info in onPartVariantApplied contains volumeMultiplier,
+        /// resource and inventory part modules will be updated to reflect the change. In such a case, the new storage volume will be
+        /// resourceVolume * volumeMultiplier.
+        /// </summary>
+        [KSPField]
+        public float resourceVolume = 0;
+        #endregion
+
+        #region Housekeeping
+        WBIOmniStorageWrapper omniStorage;
+        #endregion
+
+        #region Overrides
+        public void OnDestroy()
+        {
+            GameEvents.onVariantApplied.Remove(onVariantApplied);
+        }
+
+        public override void OnAwake()
+        {
+            GameEvents.onVariantApplied.Add(onVariantApplied);
+            omniStorage = WBIOmniStorageWrapper.GetOmniStorage(part);
+        }
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            omniStorage = WBIOmniStorageWrapper.GetOmniStorage(part);
+        }
+        #endregion
+
+        #region Helpers
+        private void onVariantApplied(Part variantPart, PartVariant variant)
+        {
+            if (variantPart != part)
+                return;
+
+            // Handle single resource (absolute value)
+            string resourceName = variant.GetExtraInfoValue(kResourceName);
+            string amtStr = variant.GetExtraInfoValue(kAmount);
+            string maxAmtStr = variant.GetExtraInfoValue(kMaxAmount);
+            updateResource(resourceName, amtStr, maxAmtStr);
+
+            // Handle multiple resources (absolute values)
+            updateVariantResources(variant.Name);
+
+            // Handle storage limit (absolute value)
+            string packedVolumeLimitStr = variant.GetExtraInfoValue(kPackedVolumeLimit);
+            updateStorageCapacity(packedVolumeLimitStr);
+
+            // Handle storage limit (relative unit volumes). ModulePartGridVariants sends this.
+            string volumeMultiplierStr = variant.GetExtraInfoValue(kVolumeMultiplier);
+            float volumeMultiplier = 0;
+            if (resourceVolume > 0 && !string.IsNullOrEmpty(volumeMultiplierStr) && float.TryParse(volumeMultiplierStr, out volumeMultiplier))
+            {
+                float updatedVolume = resourceVolume * volumeMultiplier;
+                updateStorageCapacity(updatedVolume);
+            }
+
+            //Dirty the GUI
+            MonoUtilities.RefreshContextWindows(this.part);
+        }
+
+        private void updateStorageCapacity(string packedVolumeLimitStr)
+        {
+            float packedVolumeLimit = 0;
+
+            if (string.IsNullOrEmpty(packedVolumeLimitStr) || !float.TryParse(packedVolumeLimitStr, out packedVolumeLimit))
+                return;
+
+            updateStorageCapacity(packedVolumeLimit);
+        }
+
+        private void updateStorageCapacity(float storageVolume)
+        {
+            ModuleInventoryPart inventory = null;
+
+            inventory = part.FindModuleImplementing<ModuleInventoryPart>();
+            if (inventory != null && inventory.HasPackedVolumeLimit)
+                inventory.packedVolumeLimit = storageVolume;
+
+            updateOmniStorage(storageVolume);
+        }
+
+        private void updateOmniStorage(float storageVolume)
+        {
+            if (omniStorage == null)
+                return;
+
+            omniStorage.UpdateStorageVolume(storageVolume);
+        }
+
+        private void updateVariantResources(string variantName)
+        {
+            if (!HighLogic.LoadedSceneIsEditor && !HighLogic.LoadedSceneIsFlight)
+                return;
+            ConfigNode node = getPartConfigNode();
+            if (!node.HasNode(kVariantNode))
+                return;
+            ConfigNode[] nodes = node.GetNodes(kVariantNode);
+
+            node = null;
+            for (int index = 0; index < nodes.Length; index++)
+            {
+                if (!nodes[index].HasValue(kName))
+                    continue;
+                if (nodes[index].GetValue(kName) == variantName)
+                {
+                    node = nodes[index];
+                    break;
+                }
+            }
+            if (node == null)
+                return;
+
+            if (!node.HasNode(kResourceNode))
+                return;
+            nodes = node.GetNodes(kResourceNode);
+            string amtStr = string.Empty;
+            string maxAmtStr = string.Empty;
+            string resourceName;
+            for (int index = 0; index < nodes.Length; index++)
+            {
+                node = nodes[index];
+                if (!node.HasValue(kResourceName))
+                    continue;
+
+                resourceName = node.GetValue(kResourceName);
+
+                if (node.HasValue(kAmount))
+                    amtStr = node.GetValue(kAmount);
+
+                if (node.HasValue(kMaxAmount))
+                    maxAmtStr = node.GetValue(kMaxAmount);
+
+                updateResource(resourceName, amtStr, maxAmtStr);
+            }
+        }
+
+        private void updateResource(string resourceName, string amtStr, string maxAmtStr)
+        {
+            double amount = 0;
+            double maxAmount = 0;
+
+            if (string.IsNullOrEmpty(resourceName) || !part.Resources.Contains(resourceName))
+                return;
+
+            if (HighLogic.LoadedSceneIsEditor && !string.IsNullOrEmpty(amtStr) & double.TryParse(amtStr, out amount))
+                part.Resources[resourceName].amount = amount;
+
+            if (!string.IsNullOrEmpty(maxAmtStr) & double.TryParse(maxAmtStr, out maxAmount))
+            {
+                part.Resources[resourceName].maxAmount = maxAmount;
+
+                if ((part.Resources[resourceName].amount > part.Resources[resourceName].maxAmount) || HighLogic.LoadedSceneIsEditor)
+                    part.Resources[resourceName].amount = part.Resources[resourceName].maxAmount;
+            }
+
+            MonoUtilities.RefreshContextWindows(part);
+            GameEvents.onPartResourceListChange.Fire(part);
+        }
+        #endregion
+    }
+}
+
