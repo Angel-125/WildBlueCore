1a1fd73b98593f893157cc2ef404666acfc23421
Michael Billard <angel125@spellflight.com>
2022-10-26 15:54:10 -0700

    New thermal shield module

---
+++ source/WildBlueCore/PartModules/Thermal/ModuleThermalShield.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildBlueCore.Utilities;
+
+namespace WildBlueCore.PartModules.Thermal
+{
+    public class ModuleThermalShield: BasePartModule
+    {
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            if (!HighLogic.LoadedSceneIsFlight && !HighLogic.LoadedSceneIsEditor)
+                return;
+
+            if (part.parent == null)
+                return;
+
+            part.parent.maxTemp = part.maxTemp;
+            part.parent.skinMaxTemp = part.skinMaxTemp;
+        }
+    }
+}
