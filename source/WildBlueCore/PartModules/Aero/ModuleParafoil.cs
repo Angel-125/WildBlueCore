0bfe3851cc1c0ed8c80d94965050b64d423568e5
Wild Blue Industries <angel125@spellflight.com>
2026-07-24 10:43:18 -0700

    Mk18 Ram Air Parachute initial commit

---
+++ source/WildBlueCore/PartModules/Aero/ModuleParafoil.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.IO;
+using KSP.UI.Screens;
+using KSP.Localization;
+
+namespace WildBlueCore.PartModules.Aero
+{
+    /// <summary>
+    /// Acts as a controller for ModuleLiftingSurface and ModuleControlSurface
+    /// As the parachute deploys, it gains lift and flight control- if the above part modules are found.
+    /// </summary>
+    public class ModuleParafoil: ModuleParachute
+    {
+        #region Fields
+        /// <summary>
+        /// Lift coefficient when the chute is fully retracted
+        /// </summary>
+        [KSPField]
+        public float retractedDeflectionLiftCoeff = 0;
+
+        /// <summary>
+        /// Lift coefficient when the chute is semi-deployed (not fully open)
+        /// </summary>
+        [KSPField]
+        public float semiDeployedDeflectionLiftCoeff = 8;
+
+        /// <summary>
+        /// Lift coefficient when the chute is deployed (fully open)
+        /// </summary>
+        [KSPField]
+        public float deployedDeflectionLiftCoeff = 28;
+
+        /// <summary>
+        /// Control surface's lift coefficient when the chute is fully retracted
+        /// </summary>
+        [KSPField]
+        public float retractedCtlSfcDeflectionLiftCoeff = 0;
+
+        /// <summary>
+        /// Control surface lift coefficient when the chute is semi-deployed (not fully open)
+        /// </summary>
+        [KSPField]
+        public float semiDeployedCtlSfcDeflectionLiftCoeff = 0.25f;
+
+        /// <summary>
+        /// Control surface lift coefficient when the chute is deployed (fully open)
+        /// </summary>
+        [KSPField]
+        public float deployedCtlSfcDeflectionLiftCoeff = 1.25f;
+
+        /// <summary>
+        /// Flag indicating whether or not the parafoil can be steered when in the semi-deployed state.
+        /// </summary>
+        [KSPField]
+        public bool enableControlInSemiDeploy = false;
+        #endregion
+
+        #region Housekeeping
+        ModuleLiftingSurface liftingSurface;
+        ModuleControlSurface controlSurface;
+        #endregion
+
+        #region Overrides
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            // Find the lift and control surfaces
+            liftingSurface = part.FindModuleImplementing<ModuleLiftingSurface>();
+            controlSurface = part.FindModuleImplementing<ModuleControlSurface>();
+        }
+
+        /// <summary>
+        /// After the base FixedUpdate is called, handle updates to the liftingSurface and controlSurface.
+        /// </summary>
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if (!HighLogic.LoadedSceneIsFlight)
+                return;
+
+            if (liftingSurface == null && controlSurface == null)
+                return;
+
+            float deploymentCurveTime = Mathf.Clamp01(Mathf.Pow(animTime, deploymentCurve));
+
+            switch (deploymentState)
+            {
+                case deploymentStates.SEMIDEPLOYED:
+                    SetLiftCoefficient(Mathf.Lerp(retractedDeflectionLiftCoeff, semiDeployedDeflectionLiftCoeff, deploymentCurveTime));
+
+                    SetControlSurfaceCoefficient(enableControlInSemiDeploy ? Mathf.Lerp(retractedCtlSfcDeflectionLiftCoeff, semiDeployedCtlSfcDeflectionLiftCoeff, deploymentCurveTime) : retractedCtlSfcDeflectionLiftCoeff);
+                    break;
+
+                case deploymentStates.DEPLOYED:
+                    float deployedStartControlCoeff = enableControlInSemiDeploy ? semiDeployedCtlSfcDeflectionLiftCoeff : retractedCtlSfcDeflectionLiftCoeff;
+
+                    SetLiftCoefficient(Mathf.Lerp(semiDeployedDeflectionLiftCoeff, deployedDeflectionLiftCoeff, deploymentCurveTime));
+
+                    SetControlSurfaceCoefficient(Mathf.Lerp(deployedStartControlCoeff, deployedCtlSfcDeflectionLiftCoeff, deploymentCurveTime));
+                    break;
+
+                case deploymentStates.STOWED:
+                case deploymentStates.ACTIVE:
+                case deploymentStates.CUT:
+                default:
+                    SetLiftCoefficient(retractedDeflectionLiftCoeff);
+                    SetControlSurfaceCoefficient(retractedCtlSfcDeflectionLiftCoeff);
+                    break;
+            }
+        }
+        #endregion
+
+        #region Helpers
+        private void SetLiftCoefficient(float liftCoefficient)
+        {
+            if (liftingSurface != null)
+                liftingSurface.deflectionLiftCoeff = liftCoefficient;
+        }
+
+        private void SetControlSurfaceCoefficient(float controlSurfaceLiftCoefficient)
+        {
+            if (controlSurface != null)
+                controlSurface.deflectionLiftCoeff = controlSurfaceLiftCoefficient;
+        }
+        #endregion
+    }
+}
--- source/WildBlueCore/bin/Debug/API.md
+++ source/WildBlueCore/bin/Debug/API.md
@@ -1,6 +1,32 @@
 ﻿# WildBlueCore
 
 
+# PartModules.Aero.ModuleParafoil
+            
+Acts as a controller for ModuleLiftingSurface and ModuleControlSurface As the parachute deploys, it gains lift and flight control- if the above part modules are found.
+        
+## Fields
+
+### retractedDeflectionLiftCoeff
+Lift coefficient when the chute is fully retracted
+### semiDeployedDeflectionLiftCoeff
+Lift coefficient when the chute is semi-deployed (not fully open)
+### deployedDeflectionLiftCoeff
+Lift coefficient when the chute is deployed (fully open)
+### retractedCtlSfcDeflectionLiftCoeff
+Control surface's lift coefficient when the chute is fully retracted
+### semiDeployedCtlSfcDeflectionLiftCoeff
+Control surface lift coefficient when the chute is semi-deployed (not fully open)
+### deployedCtlSfcDeflectionLiftCoeff
+Control surface lift coefficient when the chute is deployed (fully open)
+### enableControlInSemiDeploy
+Flag indicating whether or not the parafoil can be steered when in the semi-deployed state.
+## Methods
+
+
+### FixedUpdate
+After the base FixedUpdate is called, handle updates to the liftingSurface and controlSurface.
+
 # PartModules.Decals.WBIModuleDecal
             
 This part module lets you change the decal using the stock flag selector. It does so independently of the mission flag.
